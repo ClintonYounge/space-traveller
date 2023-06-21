d0ecbde84896a516ad7862cc0ea86d71f7dd0b9f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 11:52:35 -0400

    store the change from another branch

--- src/components/ReservedRocket.js
+++ src/components/ReservedRocket.js
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux';
 
 export default function JoinedMissions() {
-    const { rockets } = useSelector((state) => state.rockets);
+  const { rockets } = useSelector((state) => state.rockets);
 
-    return (
-        rockets.map((rockets) => (
-            rockets.joined ? <li key={rockets.rocketId}>{rockets.rocketName}</li> : null))
-    );
+  return (
+    rockets.map((rockets) => (
+      rockets.joined ? <li key={rockets.rocketId}>{rockets.rocketName}</li> : null))
+  );
 }
