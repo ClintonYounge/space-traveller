0050fdb1e66a2d956fffd05380f5ec61dc2c88ed
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 16:35:05 -0400

    fix imgs size

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { reserveRocket, cancelReserve } from '../redux/rockets/rocketsSlice';
+import './Rockets.css';
 
 export default function Rockets({
   rocketId,
@@ -20,13 +21,13 @@ export default function Rockets({
   return (
     <div className="flex flex-col">
       <div id={rocketId} className="flex flex-row">
-        <div className="m-4">
-          <img src={flickrImages} className="img w-72" alt="planet-img" />
+        <div className="m-4 w-640px">
+          <img src={flickrImages} className="max-w-screen-sm" alt="planet-img" />
         </div>
         <div className="m-5">
           <h2 className="text-black text-2xl font-bold">{rocketName}</h2>
           {reserved ? <span className="bg-teal-500 text-white rounded-xl font-bold p-1">Reserved</span> : <span> </span>}
-          <p>{description}</p>
+          <p className="mb-6 mr-12">{description}</p>
           {
               reserved ? <button className="border-2 border-slate-200 border-black text-slate-400 rounded-xl p-2" type="button" onClick={handleCancel}>Cancel Reservation</button> : <button className="bg-blue-600 text-white rounded-xl p-2" type="button" onClick={handleReserve}>Reserve Rocket</button>
             }
