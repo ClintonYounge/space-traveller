ca5ff55d8177858a74149b49c952a449fa269218
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 16:09:16 -0400

    fix span and name issues

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -21,11 +21,11 @@ export default function Rockets({
     <div className="flex flex-col">
       <div id={rocketId} className="flex flex-row">
         <div className="m-4">
-          <img src={flickrImages} className="img w-64 h-64" alt="planet-img" />
+          <img src={flickrImages} className="img w-72" alt="planet-img" />
         </div>
-        <div>
-          <h2 className="text-black">{rocketName}</h2>
-          <span className="bg-teal-500 text-white rounded-xl font-bold p-1">{reserved ? 'Reserved' : ''}</span>
+        <div className="m-5">
+          <h2 className="text-black text-2xl font-bold">{rocketName}</h2>
+          {reserved ? <span className="bg-teal-500 text-white rounded-xl font-bold p-1">Reserved</span> : <span> </span>}
           <p>{description}</p>
           {
               reserved ? <button className="border-2 border-slate-200 border-black text-slate-400 rounded-xl p-2" type="button" onClick={handleCancel}>Cancel Reservation</button> : <button className="bg-blue-600 text-white rounded-xl p-2" type="button" onClick={handleReserve}>Reserve Rocket</button>
