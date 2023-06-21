2c1d146cd30534c285bec717ac9be362c28c9020
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 15:46:51 -0400

    add styles for img, reserve status and button

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -20,15 +20,15 @@ export default function Rockets({
   return (
     <div className="flex flex-col">
       <div id={rocketId} className="flex flex-row">
-        <div>
-          <img src={flickrImages} className="img" alt="planet-img" />
+        <div className="m-4">
+          <img src={flickrImages} className="img w-64 h-64" alt="planet-img" />
         </div>
         <div>
-          <h2>{rocketName}</h2>
-          <p>{reserved ? 'Reserved' : ''}</p>
+          <h2 className="text-black">{rocketName}</h2>
+          <span className="bg-teal-500 text-white rounded-xl font-bold p-1">{reserved ? 'Reserved' : ''}</span>
           <p>{description}</p>
           {
-              reserved ? <button type="button" onClick={handleCancel}>Cancel Reservation</button> : <button type="button" onClick={handleReserve}>Reserve Rocket</button>
+              reserved ? <button className="border-2 border-slate-200 border-black text-slate-400 rounded-xl p-2" type="button" onClick={handleCancel}>Cancel Reservation</button> : <button className="bg-blue-600 text-white rounded-xl p-2" type="button" onClick={handleReserve}>Reserve Rocket</button>
             }
         </div>
       </div>
