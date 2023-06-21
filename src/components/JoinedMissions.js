cf4233ec4091e39ce2b0254a67c41d96f0bc1557
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 16:45:24 -0400

    add li styles for joined missions and reserved rockets in the profile page

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -5,6 +5,6 @@ export default function JoinedMissions() {
 
   return (
     missions.map((mission) => (
-      mission.joined ? <li key={mission.mission_id}>{mission.mission_name}</li> : null))
+      mission.joined ? <li className="border-inherit border-2 list-none p-5" key={mission.mission_id}>{mission.mission_name}</li> : null))
   );
 }
