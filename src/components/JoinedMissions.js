24870bf17eb9c8dd4a25e250e3152cde459a4b86
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-22 10:53:24 -0400

    restore stash to solve the conflicts

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -5,10 +5,6 @@ export default function JoinedMissions() {
 
   return (
     missions.map((mission) => (
-<<<<<<< Updated upstream
-      mission.joined ? <li className="joined-mission" key={mission.mission_id}>{mission.mission_name}</li> : null))
-=======
       mission.joined ? <li className="border-inherit border-1 list-none" key={mission.mission_id}>{mission.mission_name}</li> : null))
->>>>>>> Stashed changes
   );
 }
