1755948f78e1440a1c5c8316b468a14769de216c
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-22 10:52:03 -0400

    restore stash

--- src/components/JoinedMissions.js
+++ src/components/JoinedMissions.js
@@ -5,6 +5,10 @@ export default function JoinedMissions() {
 
   return (
     missions.map((mission) => (
+<<<<<<< Updated upstream
       mission.joined ? <li className="joined-mission" key={mission.mission_id}>{mission.mission_name}</li> : null))
+=======
+      mission.joined ? <li className="border-inherit border-1 list-none" key={mission.mission_id}>{mission.mission_name}</li> : null))
+>>>>>>> Stashed changes
   );
 }
