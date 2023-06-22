57654d04eba6c21f5b806212f9673bd65e3c316f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-21 22:59:41 -0400

    fix tests to let two pass
    Co-Authored-By: Clinton Younge <clintonyounge18@gmail.com>

--- src/__tests__/rocketsSlice.test.js
+++ src/__tests__/rocketsSlice.test.js
@@ -1,7 +1,8 @@
-import fetchRockets from '../redux/rockets/rocketsSlice';
+import rocketsReducer, { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 test('reducers', () => {
-    let state;
-    state = fetchRockets({ rockets: { rockets: [], isLoading: false, reserved: false }, missions: { missions: [], isLoading: false, joined: false } }, { type: 'rockets/fetchRockets/pending', meta: { requestId: 'NDzFUQLJDYav9nD7E6OAK', requestStatus: 'pending' } });
-    expect(state).toEqual({ rockets: { rockets: [], isLoading: true, reserved: false }, missions: { missions: [], isLoading: false, joined: false } });
+  let state;
+  state = rocketsReducer(undefined, fetchRockets.pending());
+  expect(state.isLoading).toBe(true);
 });
+
